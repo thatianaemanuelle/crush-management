cfad45bf3c0cec15a32debc7dae08562b5c77035
Thatiana Emanuelle <thatiana@yahoo.com.br>
2018-10-06 14:52:32 -0300

    MeetUp Jakeline - Node

---
+++ server/server.ts
@@ -0,0 +1,4 @@
+import App from './app';
+App.app.listen(5000, () => console.log('Agora vai!!!'));
+process.once('SIGUSR2', () => App.closeDataBaseConnection('nodemon restart', ()=> process.kill(process.pid, 'SIGUSR2')));
+process.on('SIGINT', () => App.closeDataBaseConnection('execução foi interrompida', ()=> process.exit(0)));
